b6a7e52dbe382422d13ef8c18863abc3ed33d7d6
won1lgl <2192171643@qq.com>
2019-10-24 19:15:47 +0800

    add button by code

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -9,13 +9,26 @@
 import UIKit
 
 class RatingControl: UIStackView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    //MARK:Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
     }
-    */
-
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK:Private Methods
+    private func setupButtons() {
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        //Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        //Add button to the stack
+        addArrangedSubview(button)
+    }
 }
