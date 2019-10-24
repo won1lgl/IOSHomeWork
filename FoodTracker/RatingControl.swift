4af2f130be50601ad7fd52edb5d640614acd4df2
won1lgl <2192171643@qq.com>
2019-10-24 18:46:57 +0800

    增加了自定义控制器

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Mac on 2019/10/24.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
