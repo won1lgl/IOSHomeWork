078d88e998d9c3e1efe884993984f65be38df479
won1lgl <2192171643@qq.com>
2019-10-24 14:43:44 +0800

    add IBAction and IBOutlet

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,12 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //MARK:Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        nameTextField.delegate = self
     }
+    
+    //MARK:UItextFieldDelagate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //set the label value
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK:Action
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default text"
+    }
+    
 
 
 }
