cffd43a95bccee2db6127dbb16eb8c0c5d5ec310
won1lgl <2192171643@qq.com>
2019-10-30 14:05:13 +0800

    enable to save and load Meal

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
-class Meal{
+import os.log
+class Meal: NSObject, NSCoding{
+
     //MARK:Properties
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    //MARK:Archiving paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchivedUrl = DocumentsDirectory.appendingPathComponent("meals")
+    
+    //MARK:Types
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
     
     init?(name: String, photo: UIImage?, rating: Int) {
         //validate the parameters value
@@ -25,6 +38,24 @@ class Meal{
         self.name = name
         self.photo = photo
         self.rating = rating
+    }
+    
+    //MARK:NScoding
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: PropertyKey.name)
+        coder.encode(photo, forKey: PropertyKey.photo)
+        coder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder: NSCoder) {
+        guard let name = coder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("unable to decode as a string value ", log: OSLog.default, type: .debug)
+            return nil
+        }
+        let photo = coder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        let rating = coder.decodeInteger(forKey: PropertyKey.rating)
+        self.init(name: name, photo: photo, rating: rating)
+        
     }
 }
 
